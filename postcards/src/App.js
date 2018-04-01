d99323232b4452229ccd9049b5e69f331ebb888d
Hannah <hmansoori97@gmail.com>
2018-03-31 20:42:04 -0700

    Add files

--- postcards/src/App.js
+++ postcards/src/App.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import {HashRouter} from 'react-router-dom';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = { userId: null, show: false };
+  }
+
+  //Lifecycle callback executed when the component appears on the screen.
+  //It is cleaner to use this than the constructor for fetching data
+  componentDidMount() {
+    /* Add a listener and callback for authentication events */
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log('Auth state changed: logged in as', user.email);
+        this.setState({ userId: user.uid, show: true });
+      }
+      else {
+        console.log('Auth state changed: logged out');
+        this.setState({ userId: null, show: false }); //null out the saved state
+        HashRouter.push('/login');
+      }
+    })
+  }
+
+    //A callback function for logging out the current user
+  signOut() {
+    /* Sign out the user, and update the state */
+    firebase.auth().signOut();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header className="container-fluid">
+          <div className="logo">
+          <h1>Slick</h1>
+            <i className="fa fa-cloud fa-3x" aria-label="Messageer logo"></i>
+          </div>
+          {this.state.userId &&  /*inline conditional rendering*/
+            <div className="logout">
+              <button className="btn btn-warning" onClick={() => this.signOut() }>Sign out {firebase.auth().currentUser.displayName}</button>
+            </div>
+          }
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="container">
+        {/* <div> {this.state.show ? <ChannelLinks/> : null}
+        </div> */}
+          {this.props.children}
+        </main>
       </div>
     );
   }
--- postcards/src/index.js
+++ postcards/src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+import {Route, HashRouter} from 'react-router-dom';
+import SignUpForm from './SignUpForm';
+import SignIn from './SignIn';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//load our CSS file
+import './index.css';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA5zj-oGT5jwoGY0booNAkiL-E1w1kJJTw",
+    authDomain: "postcards-71b86.firebaseapp.com",
+    databaseURL: "https://postcards-71b86.firebaseio.com",
+    projectId: "postcards-71b86",
+    storageBucket: "postcards-71b86.appspot.com",
+    messagingSenderId: "317590344118"
+  };
+  firebase.initializeApp(config);
+
+//render the application
+ReactDOM.render((
+    <HashRouter> 
+      <App/>
+    </HashRouter>),
+    document.getElementById('root'))
+  
 registerServiceWorker();
