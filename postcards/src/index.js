8f3172f3cd78f50dd851c717ad3f775671831f18
Hannah <hmansoori97@gmail.com>
2018-04-02 13:17:53 -0700

    Edit overall app structure, set up basic routing and user creation functionality

--- postcards/src/index.js
+++ postcards/src/index.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import firebase from 'firebase';
-import {Route, HashRouter} from 'react-router-dom';
-import SignUpForm from './SignUpForm';
-import SignIn from './SignIn';
+import App from './components/App'; //import our component
 
 //load our CSS file
-import './index.css';
+import './components/index.css';
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyA5zj-oGT5jwoGY0booNAkiL-E1w1kJJTw",
-    authDomain: "postcards-71b86.firebaseapp.com",
-    databaseURL: "https://postcards-71b86.firebaseio.com",
-    projectId: "postcards-71b86",
-    storageBucket: "postcards-71b86.appspot.com",
-    messagingSenderId: "317590344118"
-  };
-  firebase.initializeApp(config);
-
-//render the application
+//render the Application view
 ReactDOM.render((
-    <HashRouter> 
-      <App/>
-    </HashRouter>),
-    document.getElementById('root'))
-  
-registerServiceWorker();
+ <App />),
+  document.getElementById('root')
+);
+
+//registerServiceWorker();
