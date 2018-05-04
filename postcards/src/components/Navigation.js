c95290c7722e0b6cc033c5340edb5f457964a3f8
Patricia Au <patau@uw.edu>
2018-05-03 20:03:28 -0700

    Navigation bar; need to fix CSS

--- postcards/src/components/Navigation.js
+++ postcards/src/components/Navigation.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {Nav, Navbar, NavItem} from 'react-bootstrap'
 
 import AuthUserContext from './AuthUserContext';
 import * as routes from '../constants/routes';
 import SignOutButton from './SignOut';
+
 
 const Navigation = () =>
   <AuthUserContext.Consumer>
@@ -13,17 +15,40 @@ const Navigation = () =>
     }
   </AuthUserContext.Consumer>
   const NavigationAuth = () =>
-  <ul>
-    <li><Link to={routes.LANDING}>Landing</Link></li>
-    <li><Link to={routes.GROUP}>Groups</Link></li>
-    <li><Link to={routes.ACCOUNT}>Account</Link></li>
-    <li><SignOutButton /></li>
-  </ul>
+  // <ul>
+  //   <li><Link to={routes.LANDING}>Landing</Link></li>
+  //   <li><Link to={routes.GROUP}>Groups</Link></li>
+  //   <li><Link to={routes.ACCOUNT}>Account</Link></li>
+  //   <li><SignOutButton /></li>
+  // </ul>
+
+  <Navbar inverse >
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#home">React-Bootstrap</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav pullRight>
+      <li><Link to={routes.LANDING}>Landing</Link></li>
+      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+      </Nav>
+    </Navbar>
 
 const NavigationNonAuth = () =>
-  <ul>
+  // <ul>
+  //   <li><Link to={routes.LANDING}>Landing</Link></li>
+  //   <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+  // </ul>
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#home">React-Bootstrap</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
     <li><Link to={routes.LANDING}>Landing</Link></li>
     <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-  </ul>
+    </Nav>
+  </Navbar>
 
 export default Navigation;
